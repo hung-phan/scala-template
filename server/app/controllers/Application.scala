ae426363ad868891bcf49d3912e5aceefb55d23d
hung-phan <colorvisavn@gmail.com>
2016-10-24 00:12:45 +0700

    Finish workbench integration

--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -1,11 +1,12 @@
 package controllers
 
+import com.google.inject.Inject
+import play.api.Environment
 import play.api.mvc._
 import shared.SharedMessages
 
-class Application extends Controller {
-
+class Application @Inject()(environment: Environment) extends Controller {
   def index = Action {
-    Ok(views.html.index(SharedMessages.itWorks))
+    Ok(views.html.index(environment)(SharedMessages.itWorks))
   }
 }
