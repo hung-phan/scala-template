499d991e497e36a9549b11387790daf7497bac5e
hung-phan <colorvisavn@gmail.com>
2017-01-08 10:29:56 +0700

    Update InitialData

--- server/app/bootstrap/InitialData.scala
+++ server/app/bootstrap/InitialData.scala
@@ -4,17 +4,24 @@ import javax.inject.Inject
 
 import dao.TodosDAO
 import models.Todo
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import scala.util.Try
 
 class InitialData @Inject()(todosDAO: TodosDAO) {
+  // run on start
+
   Try(Await.result(
-    todosDAO.insert(Seq(
-      Todo("Hello", complete = true),
-      Todo("world", complete = true)
-    )),
+    todosDAO.count().map((size: Int) => {
+      if (size == 0) {
+        todosDAO.insert(
+          for (index <- 1 to 10)
+            yield Todo(s"Todo $index", complete = false)
+        )
+      }
+    }),
     Duration.Inf
   ))
 }
--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -2,17 +2,11 @@ package controllers
 
 import javax.inject.Inject
 
-import dao.TodosDAO
 import helpers.ViewHelpers
-import models.Todo
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc._
 
-class Application @Inject()(todosDAO: TodosDAO, implicit val viewHelpers: ViewHelpers) extends Controller {
-  def index: Action[AnyContent] = Action.async {
-    todosDAO.all().map((todos: Seq[Todo]) => {
-      println(todos)
-      Ok(views.html.index())
-    })
+class Application @Inject()(implicit val viewHelpers: ViewHelpers) extends Controller {
+  def index: Action[AnyContent] = Action {
+    Ok(views.html.index())
   }
 }
