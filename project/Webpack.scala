67add1fb52f7ef2345578f34cdeaf3894c300a64
hung-phan <colorvisavn@gmail.com>
2017-08-04 00:55:31 +0800

    Change build command

--- project/Webpack.scala
+++ project/Webpack.scala
@@ -16,11 +16,11 @@ object Webpack {
 
   def runDev(dir: File): Int = {
     val packagesInstall = runYarnInstall(dir)
-    if (packagesInstall == 0) Process("yarn run dev", dir) ! else packagesInstall
+    if (packagesInstall == 0) Process("npm run dev", dir) ! else packagesInstall
   }
 
   def runBuild(dir: File): Int = {
     val packagesInstall = runYarnInstall(dir)
-    if (packagesInstall == 0) Process("yarn run build", dir) ! else packagesInstall
+    if (packagesInstall == 0) Process("npm run build", dir) ! else packagesInstall
   }
 }
